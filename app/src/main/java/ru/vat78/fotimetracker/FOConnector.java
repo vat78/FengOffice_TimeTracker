6c7a74ea4422518989004c23e984596a6bbb2fe4
Vyacheslav Tikhonov <Vat78@inbox.ru>
2015-11-24 09:28:20 +0300

    Initial commit

---
+++ app/src/main/java/ru/vat78/fotimetracker/FOConnector.java
@@ -0,0 +1,406 @@
+package ru.vat78.fotimetracker;
+
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.security.KeyStore;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManagerFactory;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+/**
+ * Created by vat on 17.11.2015.
+ */
+
+// user token: 743a596acca42d631a4956232b2c755c6bd1ffe7
+
+public class FOConnector {
+
+    private static int ErrorCode=0;
+    private static String ErrorMsg="";
+
+    private static String FO_User;
+    private static String FO_Pwd;
+    private static String FO_URL;
+    private JSONObject jsonobject;
+
+    protected boolean UseUntrustCA = false;
+
+    protected boolean setFo_Url(String foUrl) {
+        foUrl = foUrl.trim();
+        if (foUrl.length() <1){ return false;}
+
+        if (!foUrl.startsWith("http://") && !foUrl.startsWith("https://")) {
+            foUrl = "https://" + foUrl;
+        }
+
+        if (!foUrl.endsWith("/")) {foUrl += "/";}
+        FO_URL = foUrl;
+        return true;
+    }
+
+    protected String getFoUrl() {
+        return FO_URL;
+    }
+
+    protected boolean setFO_User(String FO_User) {
+        FOConnector.FO_User = FO_User;
+        return true;
+    }
+
+    protected String getFO_User() {
+        return FO_User;
+    }
+
+    protected boolean setFO_Pwd(String FO_Pwd) {
+        FOConnector.FO_Pwd = FO_Pwd;
+        return true;
+    }
+
+    protected String getFO_Pwd() {
+        return FO_Pwd;
+    }
+
+    protected String getError() {
+        return ErrorMsg;
+    }
+
+    protected boolean TestConnection() {
+        //TODO this is wrong function. Just for tests. Do I need it?
+        if (TextUtils.isEmpty(FO_URL) || TextUtils.isEmpty(FO_User)) {
+            return false;
+        }
+
+        String request = FO_URL + "index.php?c=api&m=listing&srv=ProjectMessages&auth=" + FO_Pwd;
+
+        // Download JSON data from URL
+        JSONArray js = null;
+        js = JSONfunctions.getJSONArrfromURL(request, UseUntrustCA);
+
+        if (js == null) {
+            ErrorMsg = JSONfunctions.getError();
+        }
+
+        return (js != null);
+    }
+
+    protected boolean get_object(int oid) {
+        if (TextUtils.isEmpty(FO_URL) || TextUtils.isEmpty(FO_User)) {
+            return false;
+        }
+        StringBuilder sb = new StringBuilder();
+
+        String request = FO_URL + "index.php?c=api&m=get_object&oid=" + oid +"&auth=" + FO_Pwd;
+
+        jsonobject = JSONfunctions.getJSONObjfromURL(request, UseUntrustCA);
+
+        if (jsonobject == null) {
+            ErrorMsg = JSONfunctions.getError();
+        }
+        return (jsonobject != null);
+    }
+
+    protected Map get_task(int oid) {
+
+        return null;
+    }
+
+    protected ArrayList get_workspaces(){
+        return null;
+    }
+
+    protected ArrayList get_tasks(int workspace){
+        return null;
+    }
+
+    protected ArrayList get_timeslots(int task_id){
+        return null;
+    }
+
+    protected Boolean put_timeslot(ArrayList timeslot) {
+        return null;
+    }
+
+
+
+    private Void resetError(){
+        ErrorCode = 0;
+        ErrorMsg = "";
+        return null;
+    }
+
+    private static class MyTrustManager implements X509TrustManager
+    {
+
+        @Override
+        public void checkClientTrusted(X509Certificate[] chain, String authType)
+                throws CertificateException
+        {
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] chain, String authType)
+                throws CertificateException
+        {
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers()
+        {
+            return null;
+        }
+
+    }
+
+    private static class NullHostNameVerifier implements HostnameVerifier {
+
+        @Override
+        public boolean verify(String s, SSLSession sslSession) {
+            return true;
+        }
+
+    }
+
+    private static class JSONfunctions {
+
+        private static int ErrorCode=0;
+        private static String ErrorMsg="";
+
+        public static Void resetError(){
+            ErrorCode=0;
+            ErrorMsg = "";
+            return null;
+        }
+
+        public static int getErrorID() {
+            return ErrorCode;
+        }
+
+        public static String getError() {
+            return ErrorMsg;
+        }
+
+        private static String getStringFromURL(String url, boolean untrustCA) {
+
+            InputStream is = null;
+            String result = "";
+
+            // Download JSON data from URL
+            if (url.startsWith("https://")) {
+                is = getFromHTTPS(url, untrustCA);
+            } else {
+                is = getFromHTTP(url);
+            }
+            if (ErrorCode != 0) {
+                return null;
+            }
+            if (is == null) {
+                ErrorCode = 6;
+                ErrorMsg = "";
+                return null;
+            }
+
+            // Convert response to string
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+                is.close();
+
+                result = sb.toString();
+
+            } catch (Exception e) {
+                ErrorCode = 4;
+                ErrorMsg = "Error converting result";
+                Log.e("log_tag", "Error converting result " + e.toString());
+            }
+            if (result.startsWith("[")) {
+                //Add global JSON object for array
+                result = "{\"fo_obj\":" + result + "}";
+            }
+            return result;
+        }
+
+        private static Void FindErrorInData(String data) {
+            if (data == ""){
+                ErrorCode = 10;
+                ErrorMsg = "EmptyData";
+                return null;
+            }
+
+            if (data.startsWith("Fatal error")) {
+                ErrorCode = 11;
+                ErrorMsg = data;
+                return null;
+            }
+
+            if (data.startsWith("API Response")) {
+                ErrorCode = 12;
+                ErrorMsg = data;
+                return null;
+            }
+
+            if (!data.startsWith("{")) {
+                ErrorCode = 13;
+                ErrorMsg = "Wrong data format";
+            }
+
+            return null;
+        }
+
+
+        public static JSONObject getJSONObjfromURL(String url, boolean untrustCA) {
+
+            resetError();
+            JSONObject jObj = null;
+            String data = getStringFromURL(url, untrustCA);
+            FindErrorInData(data);
+            if (ErrorCode != 0) {return null;}
+
+            try {
+
+                jObj = new JSONObject(data);
+
+            } catch (JSONException e) {
+                ErrorCode = 5;
+                ErrorMsg = "Error parsing data";
+                Log.e("log_tag", "Error parsing data " + e.toString());
+            }
+
+            return jObj;
+        }
+
+        public static JSONArray getJSONArrfromURL(String url, boolean untrustCA) {
+
+            JSONArray jArr = null;
+            JSONObject jObj = getJSONObjfromURL(url, untrustCA);
+            if (ErrorCode != 0) {return null;}
+
+            try {
+                jArr = jObj.getJSONArray("fo_obj");
+            } catch (JSONException e) {
+                ErrorCode = 5;
+                ErrorMsg = "Error parsing data";
+                Log.e("log_tag", "Error parsing data " + e.toString());
+            }
+
+            return jArr;
+        }
+
+        private static InputStream getFromHTTP(String url){
+
+            InputStream content = null;
+            try {
+                URL url_obj = new URL(url);
+                URLConnection httpclient = url_obj.openConnection();
+                content = httpclient.getInputStream();
+            }
+            catch (Exception e)
+            {
+                ErrorCode = 2;
+                ErrorMsg = "Couldn't connect this URL";
+                Log.e("log_tag", "Error in http connection " + e.toString());
+            }
+            return content;
+        }
+
+        private static InputStream getFromHTTPS(String url, boolean untrustCA){
+
+            InputStream content = null;
+            try {
+                URL url_obj = new URL(url);
+                HttpsURLConnection httpsclient = (HttpsURLConnection) url_obj.openConnection();
+
+                if (untrustCA) {
+                    //Add Self-sign certificate
+                    //SSLContext context = getSelfSignedSSL(url);
+
+                    //Disable SSL checks
+                    httpsclient.setHostnameVerifier(new NullHostNameVerifier());
+                    SSLContext context = SSLContext.getInstance("TLS");
+                    TrustManager[] tmlist = {new MyTrustManager()};
+                    context.init(null, tmlist, null);
+
+                    httpsclient.setSSLSocketFactory(context.getSocketFactory());
+                }
+                content = httpsclient.getInputStream();
+            }
+            catch (Exception e)
+            {
+                if (untrustCA) {
+                    ErrorCode = 2;
+                    ErrorMsg = "Couldn't connect this URL";
+                }
+                else
+                {
+                    ErrorCode = 3;
+                    ErrorMsg = "Error in https connection. Check URL or enable using untrusted certificates";
+                }
+                Log.e("log_tag", "Error in https connection " + e.toString());
+            }
+            return content;
+        }
+
+        /* ----
+        This function dosn't work properly
+        TODO: find, how load certificate from Stream
+        ----- */
+        private static SSLContext getSelfSignedSSL(String url) throws Exception{
+            // Load CAs from an InputStream
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+
+            InputStream caInput =new BufferedInputStream(new FileInputStream("local.cer"));
+            Certificate ca;
+            try{
+                ca = cf.generateCertificate(caInput);
+                System.out.println("ca="+((X509Certificate) ca).getSubjectDN());
+            }finally{
+                caInput.close();
+            }
+
+            // Create a KeyStore containing our trusted CAs
+            String keyStoreType = KeyStore.getDefaultType();
+            KeyStore keyStore =KeyStore.getInstance(keyStoreType);
+            keyStore.load(null,null);
+            keyStore.setCertificateEntry("ca", ca);
+
+            // Create a TrustManager that trusts the CAs in our KeyStore
+            String tmfAlgorithm =TrustManagerFactory.getDefaultAlgorithm();
+            TrustManagerFactory tmf =TrustManagerFactory.getInstance(tmfAlgorithm);
+            tmf.init(keyStore);
+
+            // Create an SSLContext that uses our TrustManager
+            SSLContext context =SSLContext.getInstance("TLS");
+            context.init(null, tmf.getTrustManagers(),null);
+            return context;
+        }
+
+    }
+}
---
+++ app/src/main/java/ru/vat78/fotimetracker/LoginActivity.java
@@ -0,0 +1,340 @@
+package ru.vat78.fotimetracker;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ru.vat78.fotimetracker.FOConnector;
+
+//import static android.Manifest.permission.READ_CONTACTS;
+
+/**
+ * A login screen that offers login via login/password.
+ */
+public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+
+    private FOConnector FOApp;
+    private UserLoginCheck ULC;
+
+    // UI references.
+    private AutoCompleteTextView mURLView;
+    private AutoCompleteTextView mLoginView;
+    private EditText mPasswordView;
+    private CheckBox mUntrustCA;
+    private CheckBox mSaveCred;
+    private View mProgressView;
+    private View mLoginFormView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // Set up the login form.
+        //TODO russian translate?
+        //TODO save credentials
+        mURLView = (AutoCompleteTextView) findViewById(R.id.fo_url);
+        mLoginView = (AutoCompleteTextView) findViewById(R.id.login);
+        mUntrustCA = (CheckBox) findViewById(R.id.untrustCA);
+        mSaveCred = (CheckBox) findViewById(R.id.save_cred);
+
+        SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
+        mURLView.setText(preferences.getString(getString(R.string.prompt_url),""));
+        mLoginView.setText(preferences.getString(getString(R.string.prompt_login),""));
+        mUntrustCA.setChecked(preferences.getBoolean(getString(R.string.UseUntrustCA), false));
+
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mPasswordView.setText(preferences.getString(getString(R.string.prompt_password),""));
+        mSaveCred.setChecked(TextUtils.isEmpty(mPasswordView.getText().toString()));
+
+                mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button mSignInButton = (Button) findViewById(R.id.sign_in_button);
+        mSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+    }
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptLogin() {
+        if (ULC != null) {
+            return;
+        }
+
+        // Reset errors.
+        mURLView.setError(null);
+        mLoginView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String url = mURLView.getText().toString();
+        String login = mLoginView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        password="743a596acca42d631a4956232b2c755c6bd1ffe7";
+        //TODO how to get user token?
+
+        boolean cancel = false;
+        View focusView = null;
+        FOApp = new FOConnector();
+        ULC = new UserLoginCheck();
+
+        // Check for a empty url.
+        if (TextUtils.isEmpty(url)) {
+            mURLView.setError(getString(R.string.error_field_required));
+            focusView = mURLView;
+            cancel = true;
+        } else {
+            cancel = !FOApp.setFo_Url(url);
+            if (cancel) {
+                mURLView.setError(getString(R.string.error_incorrect_value));
+                focusView = mURLView;
+            }
+        }
+        FOApp.UseUntrustCA = mUntrustCA.isChecked();
+
+        //TODO empty password?
+        // Check for a valid password, if the user entered one.
+        if (TextUtils.isEmpty(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        } else {
+            cancel = !FOApp.setFO_Pwd(password);
+            if (cancel) {
+                mPasswordView.setError(getString(R.string.error_incorrect_value));
+                focusView = mPasswordView;
+            }
+        }
+
+        // Check for a empty login.
+        if (TextUtils.isEmpty(login)) {
+            mLoginView.setError(getString(R.string.error_field_required));
+            focusView = mLoginView;
+            cancel = true;
+        } else {
+            cancel = !FOApp.setFO_User(login);
+            if (cancel) {
+                mLoginView.setError(getString(R.string.error_incorrect_value));
+                focusView = mLoginView;
+            }
+        }
+
+        cancel = !isNetworkAvailable();
+        if (cancel){
+            mURLView.setError(getString(R.string.error_no_internet));
+            focusView = mURLView;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            ULC = null;
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            showProgress(true);
+            ULC.execute((Void) null);
+        }
+    }
+
+    private boolean isNetworkAvailable() {
+        // Using ConnectivityManager to check for Network Connection
+        ConnectivityManager connectivityManager = (ConnectivityManager) this.
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager
+                .getActiveNetworkInfo();
+        return activeNetworkInfo != null;
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return new CursorLoader(this,
+                // Retrieve data rows for the device user's 'profile' contact.
+                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
+                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
+
+                // Select only email addresses.
+                ContactsContract.Contacts.Data.MIMETYPE +
+                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
+                .CONTENT_ITEM_TYPE},
+
+                // Show primary email addresses first. Note that there won't be
+                // a primary email address if the user hasn't specified one.
+                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        List<String> emails = new ArrayList<>();
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            emails.add(cursor.getString(ProfileQuery.ADDRESS));
+            cursor.moveToNext();
+        }
+
+        addEmailsToAutoComplete(emails);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+
+    }
+
+    private interface ProfileQuery {
+        String[] PROJECTION = {
+                ContactsContract.CommonDataKinds.Email.ADDRESS,
+                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
+        };
+
+        int ADDRESS = 0;
+        int IS_PRIMARY = 1;
+    }
+
+
+    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
+        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(LoginActivity.this,
+                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
+
+        mLoginView.setAdapter(adapter);
+    }
+
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    public class UserLoginCheck extends AsyncTask<Void, Void, Boolean> {
+
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+
+            return FOApp.TestConnection();
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            ULC = null;
+            showProgress(false);
+
+            if (success) {
+                SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString(getString(R.string.prompt_url), mURLView.getText().toString());
+                editor.putString(getString(R.string.prompt_login), mLoginView.getText().toString());
+                editor.putBoolean(getString(R.string.UseUntrustCA), mUntrustCA.isChecked());
+                if (mSaveCred.isChecked()) {
+                    editor.putString(getString(R.string.prompt_password), mPasswordView.getText().toString());
+                } else {
+                    editor.putString(getString(R.string.prompt_password), "");
+                }
+                editor.commit();
+                finish();
+            } else {
+                mURLView.setError(FOApp.getError());
+                mURLView.requestFocus();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            ULC = null;
+            showProgress(false);
+        }
+    }
+}
+
